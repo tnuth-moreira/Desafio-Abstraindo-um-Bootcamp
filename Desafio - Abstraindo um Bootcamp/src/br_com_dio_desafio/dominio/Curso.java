0081e4913c0c781362e9668d8d123bd2f81aaab1
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-07-25 21:51:56 -0300

    Criando class conteudo para fazer heranca

--- Desafio - Abstraindo um Bootcamp/src/br_com_dio_desafio/dominio/Curso.java
+++ Desafio - Abstraindo um Bootcamp/src/br_com_dio_desafio/dominio/Curso.java
@@ -1,28 +1,16 @@
 package br_com_dio_desafio.dominio;
 
-public class Curso {
-  String titulo;
-  String descricao;
+public class Curso extends Conteudo {
+
   int cargaHoraria;
+
+  @Override
+  public double calcularXp() {
+    return XP_PADRAO * cargaHoraria;
+  }
 
   public Curso() {
 
-  }
-
-  public String getTitulo() {
-    return titulo;
-  }
-
-  public void setTitulo(String titulo) {
-    this.titulo = titulo;
-  }
-
-  public String getDescricao() {
-    return descricao;
-  }
-
-  public void setDescricao(String descricao) {
-    this.descricao = descricao;
   }
 
   public int getCargaHoraria() {
@@ -36,8 +24,8 @@ public class Curso {
   @Override
   public String toString() {
     return "Curso{" +
-        "Titulo: " + titulo + " | " +
-        "descricao: " + descricao + " | " +
+        "Titulo: " + getTitulo() + " | " +
+        "descricao: " + getDescricao() + " | " +
         "cargaHoraria: " + cargaHoraria +
         "}";
   }
